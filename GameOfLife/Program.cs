8740747bceaaaab9c0c181bf8a04f82a729b8445
elmarguevarra <64261171+elmarguevarra@users.noreply.github.com>
2021-10-12 14:53:53 +0800

    Add project files.

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var matrix = new bool[5, 5] {
+                { false, true, false, true, false },
+                { false, false, true, true, false },
+                { false, true, false, true, false },
+                { false, true, false, true, false },
+                { false, true, false, true, false }
+            };
+
+            //var matrix = new bool[3, 3] {
+            //        { false, true, false },
+            //        { true, true, true },
+            //        { false, true, false },
+            //};
+
+            var iteration = 11;
+            var results = EvaluateGameOfLife(matrix, iteration);
+        }
+
+        public static int[,] EvaluateGameOfLife(bool[,] lifeMatrix, int iteration)
+        {
+            var nbMaxtrix = new int[lifeMatrix.GetUpperBound(0) + 1, lifeMatrix.GetUpperBound(1) + 1];
+
+            int xLimit = lifeMatrix.GetUpperBound(0);
+            int yLimit = lifeMatrix.GetUpperBound(1);
+
+            for (int i = 0; i < iteration; i++)
+            {
+
+                for (int xctr = 0; xctr <= xLimit; xctr++)
+                {
+
+                    for (int yctr = 0; yctr <= yLimit; yctr++)
+                    {
+                        var aliveNb = 0;
+
+                        aliveNb = CheckVertically(lifeMatrix, xctr, yctr, xLimit, yLimit, aliveNb);
+                        aliveNb = CheckHorizontally(lifeMatrix, xctr, yctr, yLimit, aliveNb);
+
+                        nbMaxtrix[xctr, yctr] = aliveNb;
+                    }
+                }
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Iteration: " + i);
+                Display(lifeMatrix);
+                LifePerIteration(lifeMatrix, nbMaxtrix);
+                Display(nbMaxtrix);
+            }
+            return nbMaxtrix;
+        }
+
+        private static int CheckVertically(bool[,] lifeMatrix, int xctr, int yctr, int xLimit, int yLimit, int aliveNb)
+        {
+            if (xctr + 1 <= xLimit)
+            {
+                if (lifeMatrix[xctr + 1, yctr])
+                {
+                    aliveNb++;
+                }
+                aliveNb = CheckDiagonally(lifeMatrix, xctr + 1, yctr, yLimit, aliveNb);
+            }
+            if (xctr != 0)
+            {
+                if (lifeMatrix[xctr - 1, yctr])
+                {
+                    aliveNb++;
+                }
+                aliveNb = CheckDiagonally(lifeMatrix, xctr - 1, yctr, yLimit, aliveNb);
+            }
+            return aliveNb;
+        }
+
+        private static int CheckHorizontally(bool[,] lifeMatrix, int xctr, int yctr, int yLimit, int aliveNb)
+        {
+            if (yctr + 1 <= yLimit)
+            {
+                if (lifeMatrix[xctr, yctr + 1])
+                {
+                    aliveNb++;
+                }
+            }
+            if (yctr != 0)
+            {
+                if (lifeMatrix[xctr, yctr - 1])
+                {
+                    aliveNb++;
+                }
+            }
+
+            return aliveNb;
+        }
+
+        private static int CheckDiagonally(bool[,] lifeMatrix, int xctr, int yctr, int yLimit, int aliveNb)
+        {
+            return CheckHorizontally(lifeMatrix, xctr, yctr, yLimit, aliveNb);
+        }
+
+        private static int[,] LifePerIteration(bool[,] lifeMatrix, int[,] nbMaxtrix)
+        {
+            int xLimit = nbMaxtrix.GetUpperBound(0);
+            int yLimit = nbMaxtrix.GetUpperBound(1);
+
+            for (int xctr = 0; xctr <= xLimit; xctr++)
+            {
+                for (int yctr = 0; yctr <= yLimit; yctr++)
+                {
+                    if (lifeMatrix[xctr, yctr])
+                    {
+                        if (nbMaxtrix[xctr, yctr] == 1 || nbMaxtrix[xctr, yctr] == 0 || nbMaxtrix[xctr, yctr] >= 4)
+                        {
+                            lifeMatrix[xctr, yctr] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (nbMaxtrix[xctr, yctr] == 3)
+                        {
+                            lifeMatrix[xctr, yctr] = true;
+                        }
+                    }
+
+
+                }
+            }
+
+            return nbMaxtrix;
+        }
+
+        private static void Display(bool[,] results)
+        {
+            for (int i = 0; i <= results.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= results.GetUpperBound(1); j++)
+                {
+                    if (results[i, j])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                    Console.Write(results[i, j] + "(" + i + "," + j + ")" + " ", Console.ForegroundColor);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        private static void Display(int[,] results)
+        {
+            for (int i = 0; i <= results.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= results.GetUpperBound(1); j++)
+                {
+                    Console.Write(results[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ GameOfLife/UnitTest_Hans.cs
@@ -0,0 +1,170 @@
+﻿using Shouldly;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GameOfLife
+{
+    public class GameOfLiveV2_UnitTest_onCodeOfElmar
+    {
+        public class GameOfLiveV2_UnitTest_HardcodedBoards
+        {
+            #region Constants and other hardcoded variables
+
+            private enum TestTarget
+            {
+                All,
+            };
+
+            #endregion
+
+            #region Get mock data
+
+            public static IEnumerable<object[]> GetAllMockData()
+            {
+                yield return new object[]
+                {
+                    TestData.Sample3x3BoardA,
+                    1,
+                    TestData.Sample3x3BoardA_After1Iteration
+                };
+
+                yield return new object[]
+                {
+                    TestData.Sample3x3BoardB,
+                    2,
+                    TestData.Sample3x3BoardB_After3Iterations
+                };
+
+                yield return new object[]
+                {
+                    TestData.Sample5x5BoardA,
+                    2,
+                    TestData.Sample5x5BoardA_After2Iterations
+                };
+            }
+
+            #endregion
+
+            #region PartnerBookingSegregatorService related tests
+
+            [Theory]
+            [MemberData(nameof(GetAllMockData))]
+            [Trait("Type", "Unit Test - Domain Service")]
+            public void GameOfLifeV2_ApplyGameOfLife_ShouldProcessCorrectly(
+                bool[,] inputMatrix,
+                int numberOfIterations,
+                bool[,] expectedMatrix)
+            {
+                // Findings:
+                // NbMatrix has no handling for value of 2?
+                // lifeMatrix in code looks good, only NbMatrix might need updates, hence failing test
+
+                var outputMatrix = Program.EvaluateGameOfLife(inputMatrix, numberOfIterations);
+
+                ValidateOutputMatrix(
+                    outputMatrix, expectedMatrix, TestTarget.All);
+            }
+
+            #endregion
+
+            #region Private assertion helper methods
+
+            private void ValidateOutputMatrix(
+                int[,] outputMatrix,
+                bool[,] expectedMatrix,
+                TestTarget target = TestTarget.All)
+            {
+                // Note: Simple test only. We did not attempt to test each of the smaller methods individually.
+                if (target == TestTarget.All)
+                {
+                    outputMatrix.ShouldNotBeNull();
+                    outputMatrix.GetLength(0).ShouldBe(expectedMatrix.GetLength(0));
+                    outputMatrix.GetLength(1).ShouldBe(expectedMatrix.GetLength(1));
+
+                    // Check for values on each cell.
+                    for (int horizontalCtr = 0; horizontalCtr < outputMatrix.GetLength(0); horizontalCtr++)
+                    {
+                        for (int verticalCtr = 0; verticalCtr < outputMatrix.GetLength(1); verticalCtr++)
+                        {
+                            var outputMatrixResult = outputMatrix[horizontalCtr, verticalCtr];
+
+                            if(outputMatrixResult == 0 || 
+                               outputMatrixResult == 1 ||
+                               outputMatrixResult >= 4)
+                                expectedMatrix[horizontalCtr, verticalCtr].ShouldBe(false);
+
+                           else if(outputMatrixResult == 3)
+                                expectedMatrix[horizontalCtr, verticalCtr].ShouldBe(true);
+                        }
+                    }
+                }
+            }
+
+            #endregion
+
+            #region Test data
+
+            private static class TestData
+            {
+                #region 3x3 boards
+
+                public static bool[,] Sample3x3BoardA = new bool[,]
+                {
+                    { false, false, false },
+                    { true, true, true },
+                    { false, false, false }
+                };
+
+                public static bool[,] Sample3x3BoardA_After1Iteration = new bool[,]
+                {
+                    // Results in vertical line
+                    { false, true, false },
+                    { false, true, false },
+                    { false, true, false }
+                };
+
+                public static bool[,] Sample3x3BoardB = new bool[,]
+                {
+                    { false, true, false },
+                    { true, true, true },
+                    { false, true, false },
+                };
+
+                public static bool[,] Sample3x3BoardB_After3Iterations = new bool[,]
+                {
+                    // Results in four diagonals as populated
+                    { true, false, true },
+                    { false, false, false },
+                    { true, false, true },
+                };
+
+                #endregion
+
+                #region 5x5 boards
+
+                public static bool[,] Sample5x5BoardA = new bool[,]
+                {
+                    { false, false, false, false, false },
+                    { false, false, false, false, false },
+                    { true, true, true, true, true },
+                    { false, false, false, false, false },
+                    { false, false, false, false, false },
+                };
+
+                public static bool[,] Sample5x5BoardA_After2Iterations = new bool[,]
+                {
+                    // Results in diamond shape with empty inside
+                    { false, false, true, false, false },
+                    { false, true, false, true, false },
+                    { true, false, false, false, true },
+                    { false, true, false, true, false },
+                    { false, false, true, false, false },
+                };
+
+                #endregion
+            }
+
+            #endregion
+        }
+    }
+}
